4fb532ead8994fa3f76f78bb33d6ea98deb24a16
cchampou <clement@champouillon.com>
2022-03-19 19:48:57 +0100

    fix: controls

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -5,7 +5,7 @@ import logo from '../assets/logo-spread-shadow.png';
 function Homepage() {
   return (
     <>
-      <video id="homepage-video" autoPlay loop muted controls>
+      <video id="homepage-video" autoPlay loop muted>
         <source src={video} type="video/mp4" />
       </video>
       <img src={logo} alt="Toukan Cinema" id="homepage-logo" />
