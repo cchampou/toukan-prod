912e50c0e46c94ef61b4a8a4e00e3cf0ef70742d
cchampou <clement@champouillon.com>
2022-03-19 18:11:38 +0100

    fix: css styles

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Homepage from './pages/Homepage';
+import './styles/application.css';
 
 function App() {
   return (
--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -5,10 +5,10 @@ import logo from '../assets/toukan.jpg';
 function Homepage() {
   return (
     <>
-      <video autoPlay loop muted>
+      <video id="homepage-video" autoPlay loop muted>
         <source src={fpv} type="video/mp4" />
       </video>
-      <img src={logo} alt="Toukan Cinema" />
+      <img src={logo} alt="Toukan Cinema" id="homepage-logo" />
     </>
   );
 }
