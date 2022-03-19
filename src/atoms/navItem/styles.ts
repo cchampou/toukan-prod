e1b59293784d7cbeb2af85985a34c107e46f288a
cchampou <clement@champouillon.com>
2022-03-20 00:17:40 +0100

    fix: nav item padding

--- src/atoms/navItem/styles.ts
+++ src/atoms/navItem/styles.ts
@@ -4,6 +4,7 @@ import { NavLink } from 'react-router-dom';
 // eslint-disable-next-line import/prefer-default-export
 export const NavItem = styled(NavLink)`
   margin: 0 64px;
+  padding: 12px 32px;
   color: ${({ theme }) => theme.colors.dark};
   text-decoration: none;
 `;
