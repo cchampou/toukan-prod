94ac4a8e59d34c712b9af194a9eb130bbd90d80a
cchampou <clement@champouillon.com>
2022-03-19 19:36:10 +0100

    feat: new video

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import fpv from '../assets/fpv.mp4';
+import video from '../assets/conflu.mp4';
 import logo from '../assets/logo.png';
 
 function Homepage() {
   return (
     <>
       <video id="homepage-video" autoPlay loop muted>
-        <source src={fpv} type="video/mp4" />
+        <source src={video} type="video/mp4" />
       </video>
       <img src={logo} alt="Toukan Cinema" id="homepage-logo" />
     </>
