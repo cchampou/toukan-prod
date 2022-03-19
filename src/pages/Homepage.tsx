c2f2331eb76872c589f8eab9846a6265d884959d
cchampou <clement@champouillon.com>
2022-03-19 19:45:46 +0100

    fix: centrage

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -5,7 +5,7 @@ import logo from '../assets/logo-spread-shadow.png';
 function Homepage() {
   return (
     <>
-      <video id="homepage-video" autoPlay loop muted>
+      <video id="homepage-video" autoPlay loop muted controls>
         <source src={video} type="video/mp4" />
       </video>
       <img src={logo} alt="Toukan Cinema" id="homepage-logo" />
