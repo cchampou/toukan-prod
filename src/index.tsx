16cc0418545bf212337bcb38d13745af3076c6ce
cchampou <clement@champouillon.com>
2022-03-19 15:43:22 +0100

    chore: eslint + prettier config

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import { render } from "react-dom";
+import { render } from 'react-dom';
 import background from './assets/toukan.jpg';
 
-const App = () => {
-  return (
-    <img src={background} alt="Toukan Cinema" />
-  );
-};
+function App() {
+  return <img src={background} alt="Toukan Cinema" />;
+}
 
-render(<App />, document.getElementById("root"));
+render(<App />, document.getElementById('root'));
