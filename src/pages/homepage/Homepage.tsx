f36db201ff4d027a2aa5f5fd04bb3d7f576b6136
cchampou <clement@champouillon.com>
2022-03-20 02:04:00 +0100

    feat: use s3

--- src/pages/homepage/Homepage.tsx
+++ src/pages/homepage/Homepage.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import video from '../../assets/intro.mp4';
 import logoWhite from '../../assets/logo-white.png';
 import Scroll from '../../atoms/scroll/Scroll';
 import './styles.css';
@@ -11,7 +10,10 @@ function Homepage() {
   return (
     <>
       <video id="homepage-video" autoPlay loop muted>
-        <source src={video} type="video/mp4" />
+        <source
+          src="https://toukan-production.s3.eu-west-3.amazonaws.com/intro.mp4"
+          type="video/mp4"
+        />
       </video>
       <img
         src={logoWhite}
