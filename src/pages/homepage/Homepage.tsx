adeb7b83d0b1af2156d03ff85d3b9b8d7952e163
cchampou <clement@champouillon.com>
2022-03-22 22:31:46 +0100

    feat: video bands container

--- src/pages/homepage/Homepage.tsx
+++ src/pages/homepage/Homepage.tsx
@@ -47,15 +47,17 @@ function Homepage() {
             qualitative.
           </PresentationParagraph>
         </PresentationContainer>
-        <video ref={playerRefCorpo} className="video-band" loop muted>
-          <source src="https://toukan-production.s3.eu-west-3.amazonaws.com/corpo.mp4" />
-        </video>
-        <video ref={playerRefClip} className="video-band" loop muted>
-          <source src="https://toukan-production.s3.eu-west-3.amazonaws.com/clip.mp4" />
-        </video>
-        <video ref={playerRefDrone} className="video-band" loop muted>
-          <source src="https://toukan-production.s3.eu-west-3.amazonaws.com/drone.mp4" />
-        </video>
+        <div id="homepage-video-bands-container">
+          <video ref={playerRefCorpo} className="video-band" loop muted>
+            <source src="https://toukan-production.s3.eu-west-3.amazonaws.com/corpo.mp4" />
+          </video>
+          <video ref={playerRefClip} className="video-band" loop muted>
+            <source src="https://toukan-production.s3.eu-west-3.amazonaws.com/clip.mp4" />
+          </video>
+          <video ref={playerRefDrone} className="video-band" loop muted>
+            <source src="https://toukan-production.s3.eu-west-3.amazonaws.com/drone.mp4" />
+          </video>
+        </div>
       </ContentContainer>
     </>
   );
