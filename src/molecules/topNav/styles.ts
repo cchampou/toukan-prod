5c7efcab37090a6d84ee815126ddb65759b8f933
cchampou <clement@champouillon.com>
2022-03-25 22:23:14 +0100

    fix: topnav size

--- src/molecules/topNav/styles.ts
+++ src/molecules/topNav/styles.ts
@@ -4,7 +4,7 @@ export const Nav = styled('nav')`
   background-color: ${({ theme }) => theme.colors.light};
   position: fixed;
   width: 90%;
-  height: 48px;
+  height: 64px;
   z-index: 2;
   transition: all 0.5s ease-in-out;
   padding: 16px 5%;
