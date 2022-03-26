524f0c01dd3cf1bfb7baca161b5561d63694bbfd
cchampou <clement@champouillon.com>
2022-03-26 02:01:14 +0100

    feat: strong in texts

--- src/pages/drone/Drone.tsx
+++ src/pages/drone/Drone.tsx
@@ -9,17 +9,22 @@ function Drone() {
     <Container>
       <h1>Les Drones</h1>
       <p style={{ textAlign: 'center', width: '60%', margin: 'auto' }}>
-        Ayant passé ma licence professionnelle de télépilote de drone, je vous
-        propose d’inclure dans vos vidéos de magnifiques plans aériens. Le drone
-        est devenu un outil très convoité dans le milieu de la vidéo, que ce
-        soit pour des mariages, des clips, du cinéma, et des vidéos corporates.
+        Ayant passé ma <strong>licence professionnelle</strong> de{' '}
+        <strong>télépilote de drone</strong>, je vous propose d’inclure dans vos
+        vidéos de magnifiques plans aériens. Le drone est devenu un outil très
+        convoité dans le milieu de la vidéo, que ce soit pour des{' '}
+        <strong>mariages</strong>, des
+        <strong>clips</strong>, du <strong>cinéma</strong>, et des vidéos{' '}
+        <strong>corporates</strong>.
       </p>
       <Split>
         <p style={{ textAlign: 'right' }}>
           <h2 style={{ textAlign: 'right', margin: 0 }}>Cinématique</h2>
-          Le drone dit “Cinématique”, est tout simplement un drone stabilisé qui
-          permet de faire des images calmes et fluides et de long travelings.
-          Parfait pour de belles transitions et ouvertures de vos vidéos.
+          Le drone dit “Cinématique”, est tout simplement un drone{' '}
+          <strong>stabilisé</strong> qui permet de faire des images{' '}
+          <strong>calmes</strong> et <strong>fluides</strong> et de long{' '}
+          <strong>travelings</strong>. Parfait pour de belles transitions et
+          ouvertures de vos vidéos.
         </p>
         <Video
           type="video/mp4"
@@ -37,8 +42,9 @@ function Drone() {
         <p>
           <h2 style={{ textAlign: 'left', margin: 0 }}>FPV</h2>
           Le FPV est une catégorie bien particulière du drone qui permettra des
-          plans inédits pour vos projets. Que ce soit pour des vidéos sportives,
-          des clips ou des vidéos corporates, le FPV se démocratise de plus en
+          plans <strong>inédits</strong> pour vos projets. Que ce soit pour des
+          vidéos <strong>sportives</strong>, des <strong>clips</strong> ou des
+          vidéos <strong>corporates</strong>, le FPV se démocratise de plus en
           plus dans le milieu de l’audiovisuel. Pour en savoir plus, cliquez
           ici.
         </p>
