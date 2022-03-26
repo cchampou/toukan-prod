8448810dbc71c49010d0d2fc4337098a0e0721b9
cchampou <clement@champouillon.com>
2022-03-26 01:19:03 +0100

    feat: page drone

--- src/pages/drone/Drone.tsx
+++ src/pages/drone/Drone.tsx
@@ -1,18 +1,37 @@
 import React from 'react';
-import { Container } from '../clip/styles';
+import { Container, Split } from '../clip/styles';
 import Video from '../../molecules/video/Video';
 import droneThumbnail from '../../assets/images/thumbnails/drone.jpg';
+import liveThumbnail from '../../assets/images/thumbnails/live.jpg';
 
 function Drone() {
   return (
     <Container>
       <h1>Drones</h1>
-      <h2>Cinématique</h2>
-      <Video
-        type="video/mp4"
-        src="https://toukan-production.s3.eu-west-3.amazonaws.com/clip-demo.mp4"
-        poster={droneThumbnail}
-      />
+      <p style={{ textAlign: 'center', width: '60%', margin: 'auto' }}>...</p>
+      <Split>
+        <p style={{ textAlign: 'right' }}>
+          <h2 style={{ textAlign: 'right', margin: 0 }}>Cinématique</h2>
+          ...
+        </p>
+        <Video
+          type="video/mp4"
+          src="https://toukan-production.s3.eu-west-3.amazonaws.com/clip-demo.mp4"
+          poster={droneThumbnail}
+        />
+      </Split>
+
+      <Split>
+        <Video
+          type="video/mp4"
+          src="https://toukan-production.s3.eu-west-3.amazonaws.com/live.mp4"
+          poster={liveThumbnail}
+        />
+        <p>
+          <h2 style={{ textAlign: 'left', margin: 0 }}>FPV</h2>
+          ...
+        </p>
+      </Split>
     </Container>
   );
 }
