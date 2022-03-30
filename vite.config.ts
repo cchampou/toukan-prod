96b8b363b6576da2f61b2cbf8f025132de0b8765
cchampou <clement@champouillon.com>
2022-03-30 19:28:55 +0200

    fix: remove service workers

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,6 @@
-// eslint-disable-next-line import/no-extraneous-dependencies
-import { VitePWA } from 'vite-plugin-pwa';
-
 export default {
   server: {
     host: '0.0.0.0',
   },
-  plugins: [VitePWA()],
+  plugins: [],
 };
