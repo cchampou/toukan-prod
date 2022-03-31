6fa7652457e2979a25032460a65e49da1fea2a75
cchampou <clement@champouillon.com>
2022-03-31 21:24:29 +0200

    feat: animation on clickable thumbnails

--- src/pages/portfolio/styles.ts
+++ src/pages/portfolio/styles.ts
@@ -4,4 +4,13 @@ export const ClickableThumbnail = styled('button')`
   background-color: transparent;
   padding: 0;
   border: none;
+  overflow: hidden;
+
+  img {
+    transition: transform 0.5s ease-in-out;
+  }
+
+  &:hover img {
+    transform: scale(1.05);
+  }
 `;
