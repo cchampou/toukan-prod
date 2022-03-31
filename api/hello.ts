faa86301d98068e61dc4e7db94356f277ec32b80
cchampou <clement@champouillon.com>
2022-03-31 19:08:31 +0200

    fix: typechecking in lambda

--- api/hello.ts
+++ api/hello.ts
@@ -1,4 +1,9 @@
-export default function handler(request, response) {
+import type { VercelRequest, VercelResponse } from '@vercel/node';
+
+export default function handler(
+  request: VercelRequest,
+  response: VercelResponse
+) {
   const { name } = request.query;
   response.status(200).send(`Hello ${name}!`);
 }
