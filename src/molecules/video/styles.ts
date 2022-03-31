eff3c08aea913d013f18299e8ebf29182364f0ec
cchampou <clement@champouillon.com>
2022-03-31 19:11:19 +0200

    fix: dead code in video player

--- src/molecules/video/styles.ts
+++ src/molecules/video/styles.ts
@@ -8,25 +8,3 @@ export const Container = styled('div')`
     max-width: 100%;
   }
 `;
-
-export const Play = styled('div')`
-  height: 64px;
-  width: 64px;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  border-radius: 50%;
-  background-color: ${({ theme }) => theme.colors.darkLight};
-  color: ${({ theme }) => theme.colors.lightDark};
-  font-size: 24px;
-  position: absolute;
-  text-align: center;
-  opacity: 0.75;
-
-  svg {
-    position: relative;
-    top: 50%;
-    margin: auto;
-    transform: translateY(-50%);
-  }
-`;
